9695c2a311fe4d76860d67fb41666a6123147ac9
MilHouse10 <alstjr_96@naver.com>
2021-06-18 10:11:54 +0900

    데이터 전달하기&받아오기

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Comment } from '../';
 import './CommentList.css';
 
-const CommentList = () => {
+const CommentList = ({comments}) => {
+
+    const commentList = comments.map(
+        (comment, index) => (
+            <Comment
+                name={comment.email.split('@')[0]}
+                body={comment.body}
+                key={index}
+            />
+        )
+    );
+
     return (
         <ul className="CommentList">
-            <Comment />
-            <Comment />
-            <Comment />
+            {commentList}
         </ul>
     );
 };
