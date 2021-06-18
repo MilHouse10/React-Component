ea7c7e507f0813eee8874285c0297fe2aa92cf72
MilHouse10 <alstjr_96@naver.com>
2021-06-18 11:09:41 +0900

    경고메세지 에니메이션 구현

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
-import { PostWrapper, Navigate, Post } from '../../components';
+import { PostWrapper, Navigate, Post, Warning } from '../../components';
 import * as service from '../../services/post';
 
 class PostContainer extends Component {
+
+    handleNavigateClick = (type) => {
+        const postId = this.state.postId;
+
+        if(type === 'NEXT') {
+            this.fetchPostInfo(postId+1);
+        } else {
+            this.fetchPostInfo(postId-1);
+        }
+    }
 
     constructor(props) {
         super();
@@ -14,7 +24,8 @@ class PostContainer extends Component {
                 title: null,
                 body: null
             },
-            comments: []
+            comments: [],
+            warningVisibility: false
         };
     }
 
@@ -22,46 +33,68 @@ class PostContainer extends Component {
         this.fetchPostInfo(1);
     }
 
-    fetchPostInfo = async (postId) => {
+    showWarning = () => {
+        this.setState({
+            warningVisibility: true
+        });
 
+        setTimeout(
+            () => {
+                this.setState({
+                    warningVisibility: false
+                });
+            }, 1500
+        );
+    }
+
+    fetchPostInfo = async (postId) => {
         this.setState({
             fetching: true
         });
 
-        const info = await Promise.all([
-            service.getPost(postId),
-            service.getComments(postId)
-        ]);
+        try{
+            const info = await Promise.all([
+                service.getPost(postId),
+                service.getComments(postId)
+            ]);
 
-        const {title, body} = info[0].data;
+            const {title, body} = info[0].data;
 
-        const comments = info[1].data;
+            const comments = info[1].data;
 
-        this.setState({
-            postId,
-            post: {
-                title,
-                body
-            },
-            comments,
-            fetching: false
-        });
+            this.setState({
+                postId,
+                post: {
+                    title,
+                    body
+                },
+                comments,
+                fetching: false
+            });
+        } catch(e) {
+            this.setState({
+                fetching: false
+            });
+            this.showWarning();
+        }
     }
 
     render() {
-        const{postId, fetching, post, comments} = this.state;
+        const{postId, fetching, post, comments, warningVisibility} = this.state;
 
         return (
             <PostWrapper>
                 <Navigate 
                     postId={postId}
                     disabled={fetching}
+                    onClick={this.handleNavigateClick}
                 />
                 <Post 
                     title={post.title}
                     body={post.body}
                     comments={comments}
                 />
+                <Warning visible={warningVisibility} message="That post does not exist" />
             </PostWrapper>
         );
     }
